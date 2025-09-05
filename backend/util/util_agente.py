8742b2e4323015f2fa0b0694c60a87d67f052282
Daminin <nick.sal.a@outlook.com>
2025-09-05 15:58:33 -0500

    Backend #10  - Refactorizado la Rpta

--- backend/util/util_agente.py
+++ backend/util/util_agente.py
@@ -7,6 +7,14 @@ from langchain_openai import AzureChatOpenAI
 # Manejo de memoria del agente
 from langgraph.checkpoint.memory import InMemorySaver
 
+# Para extraer el contenido de la respuesta 
+from langchain.schema import AIMessage
+
+def _extraerContenido(respuesta):
+    if isinstance(respuesta, dict) and "output" in respuesta:
+        return respuesta["AIMessage"]
+    return respuesta
+
 def crearAgente(
     llm: AzureChatOpenAI, tools: list | None = None, contexto: str = "", memoria=None
 ):
@@ -20,12 +28,27 @@ def crearAgente(
     return agente
 
 
-def ejecutar(agente, consulta: str = "", config=None):
+def ejecutar(agente, consulta: str = "", config=None, verbose: bool = True):
     try:
         respuesta = agente.invoke(
-            {"messages": [{"role": "user", "content": consulta}]}, config=config
-        )
-        return respuesta
+                {"messages": [{"role": "user", "content": consulta}]}, config=config
+            )
+        if not verbose:            
+            return respuesta
+        else:
+            mensajes = respuesta.get("messages", [])
+
+            for m in reversed(mensajes):
+                if isinstance(m, AIMessage):
+                    tool_calls = (m.additional_kwargs or {}).get("tool_calls") or []
+                    if not tool_calls:
+                        return (m.content or "").strip()
+
+            for m in reversed(mensajes):
+                if isinstance(m, AIMessage):
+                    return (m.content or "").strip()
+                return respuesta
+            
     except Exception as e:
         print("Error en la ejecución del agente: ", e)
         return None
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from backend.util.util_sincronizacion import cargarArchivoDeCarpeta
 from backend.util.util_base_de_datos import obtenerConexionBaseDeDatos
 from backend.flow.AgentAsTools import AgentsAsTools
 
-# orq = AgentsAsTools()
+orq = AgentsAsTools()
 
-# print(orq.enviarMensaje("¿Cómo puedo crear un ticket?"))
+print(orq.enviarMensaje("¿Cómo puedo crear un ticket?"))
 
